6e2f75ebca7c32f207cce3050607f35a90406ae7
julsa <julsa06@yandex.ru>
2023-08-25 05:33:27 +0300

    refactor: review fix

--- stats-service/server/src/main/java/ru/practicum/statsservice/server/exceptions/ValidationError.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.statsservice.server.exceptions;
-
-public class ValidationError extends RuntimeException {
-    public ValidationError(String message) {
-        super(message);
-    }
-}
