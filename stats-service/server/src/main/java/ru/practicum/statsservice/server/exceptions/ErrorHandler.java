b791a9fe9e406b3ce9e10fc8f12fee6d6f1741b9
julsa <julsa06@yandex.ru>
2023-08-29 17:58:48 +0300

    fix: add ExceptionHandler to StatServer

--- stats-service/server/src/main/java/ru/practicum/statsservice/server/exceptions/ErrorHandler.java
+++ stats-service/server/src/main/java/ru/practicum/statsservice/server/exceptions/ErrorHandler.java
@@ -1,12 +1,14 @@
 package ru.practicum.statsservice.server.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(final ValidationException e) {
         return new ErrorResponse(
@@ -14,6 +16,7 @@ public class ErrorHandler {
         );
     }
 
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(final Exception e) {
         return new ErrorResponse(
